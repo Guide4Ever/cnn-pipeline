a1a594d5c3a4a4617b18d6e3aa7f3b699634efce
Amadej Šenk <guide5ever@gmail.com>
2023-04-12 20:48:51 +0200

    Added a small change

--- utility/filter.py
+++ utility/filter.py
@@ -5,9 +5,9 @@ copy_files_containing_mlo(source_dir, dest_dir, OVERWRITE=False):
   OVERWRITE = if true, then it does not delete the folder but rather goes into it and replaces the already existing file
 """
 
-def copy_files_containing_mlo(source_dir, dest_dir, OVERWRITE=False):
+def copy_files_containing_mlo(source_dir, dest_dir, OVERWRITE_IMAGES=False):
 
-  if OVERWRITE:
+  if OVERWRITE_IMAGES:
     os.mkdir(dest_dir) if not os.path.exists(dest_dir) else None
   else:
     shutil.rmtree(dest_dir, ignore_errors=True) if os.path.exists(dest_dir) else None
